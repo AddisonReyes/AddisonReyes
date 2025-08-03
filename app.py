d182f9880a509891f4ad4049d247362378223505
Addison Reyes <106483642+AddisonReyes@users.noreply.github.com>
2025-08-02 23:00:41 -0400

    dynamic project list

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
+import json
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+def load_projects():
+    with open("data/projects.json", "r") as file:
+        projects = json.load(file)
+    return projects
+
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    projects_list = load_projects()
+    return render_template("index.html", projects=projects_list)
+
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+
+@app.route("/experience")
+def experience():
+    return render_template("experience.html")
 
 
 if __name__ == "__main__":
