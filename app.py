da08dbd460f5b31e34ca6fc38a759e1305711cc6
Addison Reyes <106483642+AddisonReyes@users.noreply.github.com>
2025-08-04 20:34:43 -0400

    Few changes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-def load_projects():
+def load_projects() -> list:
     with open("data/projects.json", "r") as file:
         projects = json.load(file)
     return projects
@@ -13,18 +13,16 @@ def load_projects():
 
 @app.route("/")
 def index():
-    projects_list = load_projects()
+    projects_list: list = load_projects()
+    if len(projects_list) > 3:
+        projects_list = projects_list[:3]
     return render_template("index.html", projects=projects_list)
 
 
 @app.route("/projects")
 def projects():
-    return render_template("projects.html")
-
-
-@app.route("/experience")
-def experience():
-    return render_template("experience.html")
+    projects_list: list = load_projects()
+    return render_template("projects.html", projects=projects_list)
 
 
 if __name__ == "__main__":
