3f5aceee062220864817cc1e0356178583ad6dd3
Addison Reyes <106483642+AddisonReyes@users.noreply.github.com>
2026-02-19 00:48:47 -0400

    Fixing route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -75,7 +75,7 @@ const ContactPage = () => (
 
 export default function App() {
   return (
-    <Router basename="/AddisonReyes/">
+    <Router basename="/">
       <ScrollToTop />
       <div className="min-h-screen flex flex-col bg-[#121212]">
         <Navbar />
